c1583f1f101e59c0565b6973aac0aca24526124a
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 20:14:27 +0200

    Finished task 2.7

--- 2.1/uzduotis/02_07.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ 2.1/uzduotis/02_07.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class MultiplicationFormula {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner calc = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(calc.nextLine());
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(calc.nextLine());
+        calc.close();
+        long result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
 
         // Write your program here
 
