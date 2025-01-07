da4ba014b9d057d299643056afdb84c10ddf2918
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 16:23:07 +0200

    Finished task 2.6

--- 2.1/uzduotis/02_06.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ 2.1/uzduotis/02_06.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class SumOfThreeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner calc = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(calc.nextLine());
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(calc.nextLine());
+        System.out.println("Give the third number:");
+        int number3 = Integer.parseInt(calc.nextLine());
+        calc.close();
+        int sum = number1 + number2 + number3;
+        System.out.println("The sum of the numbers is " + sum);
 
         // Write your program here
 
