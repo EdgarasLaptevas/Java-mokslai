8f27f950446505c541355a927573330f4e2c41a4
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 15:50:05 +0200

    Finished task 2.3

--- 2.1/uzduotis/02_03.DoubleInput/src/main/java/DoubleInput.java
+++ 2.1/uzduotis/02_03.DoubleInput/src/main/java/DoubleInput.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 public class DoubleInput {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Give a number:");
+        double giveNumber = Double.parseDouble(reader.nextLine());
+        reader.close();
+        System.out.println("You gave the number " + giveNumber);
 
         // write your program here
 
