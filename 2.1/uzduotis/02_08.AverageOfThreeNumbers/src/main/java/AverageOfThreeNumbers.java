c6f556b9474919358df2f767c6982df556eff1be
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 20:40:20 +0200

    Finished task 2.8

--- 2.1/uzduotis/02_08.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ 2.1/uzduotis/02_08.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class AverageOfThreeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner calc = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(calc.nextLine());
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(calc.nextLine());
+        System.out.println("Give the third number:");
+        int number3 = Integer.parseInt(calc.nextLine());
+        calc.close();
+        double numAverage = (double) (number1 + number2 + number3) / 3;
+        System.out.println("The average is " + numAverage);
 
         // Write your program here
 
