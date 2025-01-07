2148a5ed1abf5153d9f767b0d9f74f9f011669e8
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 15:46:33 +0200

    Finished task 2.2

--- 2.1/uzduotis/02_02.IntegerInput/src/main/java/IntegerInput.java
+++ 2.1/uzduotis/02_02.IntegerInput/src/main/java/IntegerInput.java
@@ -5,7 +5,10 @@ public class IntegerInput {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+        System.out.println("Give a number:");
+        int giveNumber = Integer.parseInt(reader.nextLine());
+        reader.close();
+        System.out.println("You gave the number " + giveNumber);
 
         // write your program here
 
