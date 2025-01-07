288e245b27c099f9242e7cb6b3bcaf2450d1ef0a
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-07 14:48:13 +0200

    Finished task4

--- 1.2/uzduotis/01_04.Greeting/src/main/java/Greeting.java
+++ 1.2/uzduotis/01_04.Greeting/src/main/java/Greeting.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 public class Greeting {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String message = reader.nextLine();
+        reader.close();
+        System.out.println("Hi" + ' ' + message);
 
         // Write your program here
 
