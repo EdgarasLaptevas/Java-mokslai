fe4e50c8500d6867cb9fb74e853cc3fb6c9267aa
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 23:10:51 +0200

    Finished 04_14 task

--- 4.2/uzduotis/04_14.SumOfASequence/src/main/java/SumOfASequence.java
+++ 4.2/uzduotis/04_14.SumOfASequence/src/main/java/SumOfASequence.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class SumOfASequence {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Last number? ");
+        int lastNumber = Integer.parseInt(reader.nextLine());
+        reader.close();
+        int sum = 0;
+        for (int i = 1; i <= lastNumber; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
 
     }
 }
