73cb83943c5f7ed59fe3a56b5049d94796145fb8
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 18:04:14 +0200

    Finished Uzduotis2_08.05 task

--- Uzduotis2_08.01/uzduotis/04_05.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ Uzduotis2_08.01/uzduotis/04_05.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(reader.nextLine());
+        int countNegatives = 0;
+        while (true)
+            if (number < 0) {
+                countNegatives += 1;
+                System.out.println("Give a number:");
+                number = Integer.parseInt(reader.nextLine());
+            } else if (number > 0) {
+                System.out.println("Give a number:");
+                number = Integer.parseInt(reader.nextLine());
+            } else {
+                System.out.println("Number of negative numbers: " + countNegatives);
+                break;
+            }
 
     }
 }
