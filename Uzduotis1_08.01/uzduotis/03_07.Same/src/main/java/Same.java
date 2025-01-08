86e0915164fd9c7594240d7f056cb0d61678e56f
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 14:11:10 +0200

    Finished Uzduotis1_08.07 task

--- Uzduotis1_08.01/uzduotis/03_07.Same/src/main/java/Same.java
+++ Uzduotis1_08.01/uzduotis/03_07.Same/src/main/java/Same.java
@@ -1,10 +1,22 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Same {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter the first string");
+        String string1 = read.nextLine();
+        System.out.println("Enter the second string");
+        String string2 = read.nextLine();
+        read.close();
+        if (string1.equals(string2)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+
 
         // Write your program here. 
     }
