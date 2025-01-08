543508b78f8817dad5733f2a83acec014ff7dfcf
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 22:52:29 +0200

    Finished 4_12 task

--- 4.2/uzduotis/04_12.CountingToHundred/src/main/java/CountingToHundred.java
+++ 4.2/uzduotis/04_12.CountingToHundred/src/main/java/CountingToHundred.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 public class CountingToHundred {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println();
+        int number = Integer.parseInt(reader.nextLine());
+        reader.close();
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
 
     }
 }
