7c9d1c623330c932e0198793a5044e11e0f6e162
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 21:55:52 +0200

    Finished Uzduotis2_08.09 task

--- Uzduotis2_08.01/uzduotis/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Uzduotis2_08.01/uzduotis/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -4,7 +4,23 @@ import java.util.Scanner;
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(reader.nextLine());
+        int countPositive = 0;
+        int sumPositive = 0;
+        while (number != 0) {
+            if (number > 0) {
+                countPositive += 1;
+                sumPositive += number;
+            }
+            System.out.println("Give a number:");
+            number = Integer.parseInt(reader.nextLine());
+        }
+        if (countPositive == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) sumPositive / countPositive);
+        }
     }
 }
