0acb56b1c8d3d74d6fefe53401a6ec07825311de
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 23:01:29 +0200

    Finished 04_13 task

--- 4.2/uzduotis/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ 4.2/uzduotis/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class FromWhereToWhere {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Where to? ");
+        int whereTo = Integer.parseInt(reader.nextLine());
+        System.out.println("Where from? ");
+        int whereFrom = Integer.parseInt(reader.nextLine());
+        reader.close();
+        for (int i = whereFrom; i <= whereTo; i++) {
+            System.out.println(i);
+        }
 
         // Write your program here
     }
