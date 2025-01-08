34dbcef7abedc44bbfa7e8662c9db05331da49ad
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 13:44:45 +0200

    Finished Uzduotis1_08.06 task

--- Uzduotis1_08.01/uzduotis/03_06.Password/src/main/java/Password.java
+++ Uzduotis1_08.01/uzduotis/03_06.Password/src/main/java/Password.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class Password {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Password?");
+        String password = read.nextLine();
+        read.close();
+        if (password.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
 
         // Write your program here 
     }
