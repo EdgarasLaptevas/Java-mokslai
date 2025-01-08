508027428ccc2b8659e6a1685ff58992973e6ca1
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 17:50:20 +0200

    Finished Uzduotis2_08.03-04 tasks

--- Uzduotis2_08.01/uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
+++ Uzduotis2_08.01/uzduotis/04_03.OnlyPositives/src/main/java/OnlyPositives.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 public class OnlyPositives {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(reader.nextLine());
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+            } else if (number > 0) {
+                int powerNumber = (int) Math.pow(number, 2);
+                System.out.println(powerNumber);
+            } else {
+                break;
+            }
 
+
+        }
     }
 }
--- Uzduotis2_08.01/uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ Uzduotis2_08.01/uzduotis/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 public class NumberOfNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Give a number");
+        int number = Integer.parseInt(reader.nextLine());
+        int count = 0;
+        while (number != 0) {
+            count += 1;
+            System.out.println("Give a number");
+            number = Integer.parseInt(reader.nextLine());
+            if (number == 0) {
+                System.out.println("Number of numbers: " + count);
+            }
+
+        }
+
 
     }
 }
