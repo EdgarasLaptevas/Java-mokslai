09114f701bb58719628ec0eebca94a1506d8811a
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 10:59:28 +0200

    Finished Uzduotis1_08.03 task

--- Uzduotis1_08.01/uzduotis/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ Uzduotis1_08.01/uzduotis/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int number1 = Integer.parseInt(read.nextLine());
+        System.out.println("Give the second number:");
+        int number2 = Integer.parseInt(read.nextLine());
+        read.close();
+        if (number1 > number2) {
+            System.out.println("Greater number is: " + number1);
+        } else if (number1 == number2) {
+            System.out.println("The number are equal!");
+        } else {
+            System.out.println("Greater number is: " + number2);
+        }
+
 
     }
 }
