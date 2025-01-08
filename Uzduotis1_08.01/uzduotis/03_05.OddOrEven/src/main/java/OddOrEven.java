2dee7151dc5159a06dfc64a77171d2ec51f7e9a3
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 13:33:57 +0200

    Finished Uzduotis1_08.05 task

--- Uzduotis1_08.01/uzduotis/03_05.OddOrEven/src/main/java/OddOrEven.java
+++ Uzduotis1_08.01/uzduotis/03_05.OddOrEven/src/main/java/OddOrEven.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class OddOrEven {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(read.nextLine());
+        read.close();
+        if (number % 2 == 0) {
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
 
         // Write your program here 
         // HINT:
