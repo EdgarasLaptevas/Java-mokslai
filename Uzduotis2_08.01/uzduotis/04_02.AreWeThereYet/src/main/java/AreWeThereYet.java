ba77bc91852b2ed26f6f06f40a3af0c989cbc15c
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 16:53:46 +0200

    Finished Uzduotis2_08.02 task

--- Uzduotis2_08.01/uzduotis/04_02.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ Uzduotis2_08.01/uzduotis/04_02.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class AreWeThereYet {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(reader.nextLine());
+        while (number != 4) {
+            System.out.println("Give a number:");
+            number = Integer.parseInt(reader.nextLine());
+        }
+
 
     }
 }
