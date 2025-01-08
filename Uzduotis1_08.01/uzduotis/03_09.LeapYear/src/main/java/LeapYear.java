4d4498daaa6efc7b3af46f928359dff86e5d4d41
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-08 16:15:23 +0200

    Finished Uzduotis1_08.09-10 tasks

--- Uzduotis1_08.01/uzduotis/03_09.LeapYear/src/main/java/LeapYear.java
+++ Uzduotis1_08.01/uzduotis/03_09.LeapYear/src/main/java/LeapYear.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class LeapYear {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner check = new Scanner(System.in);
+        System.out.println("Give a year:");
+        int year = Integer.parseInt(check.nextLine());
+        check.close();
+
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            System.out.println("The year is a leap year");
+        } else {
+            System.out.println("The year is not a leap year");
+        }
 
     }
 }
