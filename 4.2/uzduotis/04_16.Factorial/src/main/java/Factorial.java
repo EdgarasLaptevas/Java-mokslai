12085a7e45dc2407d8ec9954f898bf8a1e0291f0
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-09 08:26:31 +0200

    Finished 04_16 task

--- 4.2/uzduotis/04_16.Factorial/src/main/java/Factorial.java
+++ 4.2/uzduotis/04_16.Factorial/src/main/java/Factorial.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class Factorial {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Give a number ");
+        int number = Integer.parseInt(reader.nextLine());
+        reader.close();
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial: " + factorial);
     }
 }
